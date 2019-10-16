5cbdade4fb9151f5a149b10138b1c7a21a576fc1
EthanKwon <ethkwon94@gmail.com>
2019-10-16 16:35:36 +0900

    Route Link

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import { Home, About} from './pages';
+import { Home, About, Posts} from './pages';
+import Menu from './components/Menu';
+
 
 
 const App = () => {
   return(
     <div>
+      <Menu/>
       <Route exact path="/" component={Home}/>
       <Route path="/about/:name?" component={About}/>
+      <Route path="/posts" component={Posts}/>
     </div>
   );
 };
