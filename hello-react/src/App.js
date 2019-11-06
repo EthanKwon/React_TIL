c7c0be8050f8dd99028be87db29091ad93224ea2
EthanKwon <ethkwon94@gmail.com>
2019-11-06 16:50:50 +0900

    Add React ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import EventPracticeFunction from './EventPracticeFunction';
+import React, {Component} from 'react';
+import ScrollBox from './ScrollBox';
 
-const App = () =>{
-  return <EventPracticeFunction/>;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref = {(ref) =>this.scrollBox=ref}/>
+        <button onClick={()=> this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
