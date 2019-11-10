d36b7b3ddccc24f4e812653df8fe4ac997e8bbfb
EthanKwon <ethkwon94@gmail.com>
2019-11-10 20:01:02 +0900

    Add React CSS styling : styled-component

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import CSSModule from "./CSSModule";
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div>
-      <CSSModule/>
+      <StyledComponent/>
     </div>
-  )
+  );
 }
 
 export default App;
