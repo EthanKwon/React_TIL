41335fa692e31d50dd4e9524ff865776d0ba63f7
EthanKwon <ethkwon94@gmail.com>
2019-11-21 17:04:02 +0900

    Add React Redux : Counter useSelector and useDispatch

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React, {useCallback} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import Counter from '../components/Counter';
 import {increase, decrease} from '../modules/counter';
-import { bindActionCreators } from '../../../vanilla-redux/node_modules/redux';
 
-const CounterContaioner = ({number, increase, decrease}) => {
-    return <Counter number={number} onIncrease={increase} onDecrease={decrease}/>;
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(()=> dispatch(increase()),[dispatch]);
+    const onDecrease = useCallback(()=> dispatch(decrease()),[dispatch]);
+    return(
+        <Counter 
+            number={number}
+            onIncrease={onIncrease}
+            onDecrease={onDecrease}
+        />
+    );
 };
 
-
-export default connect(
-    state => ({
-        number : state.counter.number,
-    }),
-    dispatch =>
-        bindActionCreators(
-            {
-                increase,
-                decrease,
-            },
-            dispatch,
-        ),
-)(CounterContaioner);
+export default CounterContainer;
 
